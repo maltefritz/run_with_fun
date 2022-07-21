23aacb9fc29ed9b9b020d394c17f616deb7e48d3
maltefritz <malte.fritz@web.de>
2022-07-21 15:11:40 +0200

    Add visualisation for running analysis.

--- app.py
+++ app.py
@@ -1,15 +1,10 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Fri Jul  8 09:11:32 2022
-
-@author: Malte Fritz
-"""
-
 import streamlit as st
 import json
 import pandas as pd
 import numpy as np
-import matplotlib.dates as dt
+import matplotlib.pyplot as plt
+# import matplotlib.dates as dt
+# from datetime import datetime, time, timedelta
 
 
 # @st.cache
@@ -66,24 +61,142 @@ if app == 'Laufanalyse':
         df = get_data(user_file)
 
         with st.expander(f'Durchschnittliche Statistik', expanded=True):
-            act_ave = len(df.columns)
-            dis_ave = df['distance'].sum()/len(df.columns)
-            rt_ave = df['run time'].sum()/len(df.columns)
-            pac_ave = df['pace'].sum()/len(df.columns)
-            spe_ave = df['speed'].sum()/len(df.columns)
+            act_ave = len(df.index)
+            dis_ave = df['distance'].sum()/len(df.index)
+            rt_ave = df['run time'].sum()/len(df.index)
+            pac_ave = df['pace'].sum()/len(df.index)
+            spe_ave = df['speed'].sum()/len(df.index)
 
-            st.write(f'Anzahl Aktivitäten: {act_ave}')
-            st.write(f'Durchschnittliche Distanz: {dis_ave:.2f} km')
-            st.write(f'Durchschnittliche Laufzeit: {rt_ave:.2f} min')
-            st.write(f'Durchschnittliches Tempo: {pac_ave:.2f} min/km')
-            st.write(f'Durchschnittliche Geschwindigkeit: {spe_ave:.2f} km/h')
+            st.write(f'🏃🏻 Anzahl Aktivitäten: {act_ave}')
+            st.write(f'🛣️ Durchschnittliche Distanz: {dis_ave:.2f} km')
+            st.write(f'⏱️ Durchschnittliche Laufzeit: {rt_ave:.2f} min')
+            st.write(f'🔥 Durchschnittliches Tempo: {pac_ave:.2f} min/km')
+            st.write(
+                f'🚀 Durchschnittliche Geschwindigkeit: {spe_ave:.2f} km/h'
+                )
 
         with st.expander(f'Alle Statistik', expanded=True):
-            st.write(
-                'Statistiken: kum. Distanzen(Links), kum. Dauer(Links), '
-                + 'Punkte/Balken: Pace(rechts), Geschwindigkeiten(rechts)',
-                key=f's2'
+            # Zeitraum auswählbar machen
+            # 2 Spalten
+            # 2 kum Plots + 2 Balken
+
+            period = st.date_input(
+                'Zeitraum auswählen:', value=(df.index[0], df.index[-1]),
+                min_value=df.index[0], max_value=df.index[-1]
                 )
+
+            tab1, tab2, tab3, tab4 = st.tabs(['dis', 'dau', 'Tem', 'Spe'])
+            with tab1:
+                col1, col2 = st.columns(2)
+                with col1:
+                    st.subheader('Distanz')
+
+                    fig1s, ax = plt.subplots(figsize=(16, 9))
+                    ax.scatter(
+                        df.index,
+                        df.loc[period[0]:period[-1], 'distance'].cumsum(),
+                        linewidths=10
+                        )
+                    ax.plot(df.loc[period[0]:period[-1], 'distance'].cumsum())
+                    ax.set_xlabel('Datum')
+                    ax.set_ylabel('kummlierte km')
+                    ax.grid(linestyle='--')
+                    st.pyplot(fig1s)
+
+                    st.subheader('Dauer')
+
+                with col2:
+                    st.subheader('🛣️')
+                    fig1b, ax = plt.subplots(figsize=(16, 9))
+                    ax.bar(
+                        df.index, df.loc[period[0]:period[-1], 'distance'],
+                        width=0.5
+                        )
+                    ax.set_xlabel('Datum')
+                    ax.set_ylabel('km')
+                    ax.grid(linestyle='--')
+                    st.pyplot(fig1b)
+
+            #     fig2s, ax = plt.subplots(figsize=(16, 9))
+            #     ax.scatter(
+            #         df.index,
+            #         df.loc[period[0]:period[-1], 'run time'].cumsum(),
+            #         linewidths=10
+            #         )
+            #     ax.plot(df.loc[period[0]:period[-1], 'run time'].cumsum())
+            #     ax.set_xlabel('Datum')
+            #     ax.set_ylabel('kummlierte Stunden')
+            #     ax.grid(linestyle='--')
+            #     st.pyplot(fig2s)
+
+            #     st.subheader('Tempo')
+
+            #     fig3s, ax = plt.subplots(figsize=(16, 9))
+            #     ax.scatter(
+            #         df.index,
+            #         df.loc[period[0]:period[-1], 'pace'], linewidths=10
+            #         )
+            #     ax.plot(df.loc[period[0]:period[-1], 'pace'])
+            #     ax.set_xlabel('Datum')
+            #     ax.set_ylabel('$min/km$')
+            #     ax.grid(linestyle='--')
+            #     st.pyplot(fig3s)
+
+            #     st.subheader('Geschwindigkeit')
+
+            #     fig4s, ax = plt.subplots(figsize=(16, 9))
+            #     ax.scatter(
+            #         df.index,
+            #         df.loc[period[0]:period[-1], 'speed'], linewidths=10
+            #         )
+            #     ax.plot(df.loc[period[0]:period[-1], 'speed'])
+            #     ax.set_xlabel('Datum')
+            #     ax.set_ylabel('km/h')
+            #     ax.grid(linestyle='--')
+            #     st.pyplot(fig4s)
+
+            # with col2:
+            #     st.subheader('🛣️')
+            #     fig1b, ax = plt.subplots(figsize=(16, 9))
+            #     ax.bar(
+            #         df.index, df.loc[period[0]:period[-1], 'distance'],
+            #         width=0.5
+            #         )
+            #     ax.set_xlabel('Datum')
+            #     ax.set_ylabel('km')
+            #     ax.grid(linestyle='--')
+            #     st.pyplot(fig1b)
+
+            #     st.subheader('⏱️')
+            #     fig2b, ax = plt.subplots(figsize=(16, 9))
+            #     ax.bar(
+            #         df.index, df.loc[period[0]:period[-1], 'run time'],
+            #         width=0.5
+            #         )
+            #     ax.set_xlabel('Datum')
+            #     ax.set_ylabel('Stunden')
+            #     ax.grid(linestyle='--')
+            #     st.pyplot(fig2b)
+
+            #     st.subheader('🔥')
+            #     fig3b, ax = plt.subplots(figsize=(16, 9))
+            #     ax.bar(
+            #         df.index, df.loc[period[0]:period[-1], 'pace'], width=0.5
+            #         )
+            #     ax.set_xlabel('Datum')
+            #     ax.set_ylabel(r'$min/km$')
+            #     ax.grid(linestyle='--')
+            #     st.pyplot(fig3b)
+
+            #     st.subheader('🚀')
+            #     fig4b, ax = plt.subplots(figsize=(16, 9))
+            #     ax.bar(
+            #         df.index, df.loc[period[0]:period[-1], 'speed'], width=0.5
+            #         )
+            #     ax.set_xlabel('Datum')
+            #     ax.set_ylabel(r'$km/h$')
+            #     ax.grid(linestyle='--')
+            #     st.pyplot(fig4b)
 
         with st.expander(f'Rekorde', expanded=True):
             st.write(
